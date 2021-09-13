e23d48ed0eb3e1e1ab776bbaf17bc004ad85a1d3
chima94 <chimanwakis@gmail.com>
2021-09-13 22:04:14 +0100

    deployed

--- server.js
+++ server.js
@@ -22,12 +22,12 @@ app.use('/api/auth', require('./routes/api/auth'))
 if(process.env.NODE_ENV === 'production'){
     //set the static folder
     app.use(express.static('client/build'))
-    
+
     app.get('*', (req, res) =>{
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
-
+//
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () =>{
